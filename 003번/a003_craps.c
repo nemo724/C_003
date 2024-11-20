949a8eca44d1ad26fa0a3e03966eb4a3c6498332
nemo <nemo724@naver.com>
2024-11-20 11:15:45 +0900

    ouput_result의 prob 연산 수정

--- 003번/a003_craps.c
+++ 003번/a003_craps.c
@@ -24,8 +24,8 @@ void output_result(int cnt[])
 
  printf("결과\n");
 for(int i=2;i<=12;i++)
-    {prob= (double)cnt[i]/30;
-     printf("%d :%2d 확률 %5.2lf%%\n",i,cnt[i],prob*100); }
+    {prob= (double)cnt[i]/30*100;
+     printf("%d :%2d 확률 %5.2lf%%\n",i,cnt[i],prob); }
 }
 
 
