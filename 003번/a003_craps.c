e411d4432874dfb97eb13f6cba727eb734afb555
nemo <nemo724@naver.com>
2024-11-20 10:42:48 +0900

    실행한 주사위 눈 합들의 결과와 확률을 출력하는 함수  output_result 추가

--- 003번/a003_craps.c
+++ 003번/a003_craps.c
@@ -6,6 +6,7 @@ int rand_min = 1;
 int rand_max = 6;
 
 void make_rand(int*, int[]);
+void output_result(int[]);
 
 
 void make_rand(int* pdice, int cnt[])
@@ -13,7 +14,14 @@ void make_rand(int* pdice, int cnt[])
 *pdice+= rand() % (rand_max - rand_min + 1) + rand_min;
 cnt[*pdice] += 1;}
 
+void output_result(int cnt[])
+{double prob;
 
+printf("결과\n");
+for(int i=2;i<=12;i++)
+	{prob=cnt[i]/30;
+	printf("%d :%2d 확률 %5.2lf%%%c",i,cnt[i],prob,i%4==0?'\n':' '); }
+}
 
 
 
@@ -28,7 +36,7 @@ srand((unsigned int)time(0));
 for (int i = 0; i < 30; i++)
 {make_rand(&dice, cnt);}
 
-
+output_result(cnt);
 
 
 
