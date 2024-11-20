e47b4d95b94bf38c1546d9760f9a47b6cd326f9b
nemo <nemo724@naver.com>
2024-11-20 10:32:26 +0900

    주사위 최소,최댓값을 담은 전역변수 rand_min,max 추가 및 두 주사위 합을 담는 변수 dice 추가

--- 003번/a003_craps.c
+++ 003번/a003_craps.c
@@ -2,25 +2,14 @@
 #include <time.h>
 #include <stdlib.h>
 
-
-
-
-
-
-
-
-
-
-
-
-
+int rand_min = 1;
+int rand_max = 6;
 
 int main(void)
 {int cnt[13] = { 0 };
+int dice;
 
-
-
-
+srand((unsigned int)time(0));
 
 
 
