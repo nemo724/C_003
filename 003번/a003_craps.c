9e143919d46b5b7b45ff85fe27459d2561cd9ac3
nemo <nemo724@naver.com>
2024-11-20 10:35:28 +0900

    두 주사위의 값과 합을 구하고 결과를 기록하는 함수 make_rand추가

--- 003번/a003_craps.c
+++ 003번/a003_craps.c
@@ -5,12 +5,28 @@
 int rand_min = 1;
 int rand_max = 6;
 
+void make_rand(int*, int[]);
+
+
+void make_rand(int* pdice, int cnt[])
+{*pdice = rand() % (rand_max - rand_min + 1) + rand_min;
+*pdice+= rand() % (rand_max - rand_min + 1) + rand_min;
+cnt[*pdice] += 1;}
+
+
+
+
+
+
+
 int main(void)
 {int cnt[13] = { 0 };
 int dice;
 
 srand((unsigned int)time(0));
 
+for (int i = 0; i < 30; i++)
+{make_rand(&dice, cnt);}
 
 
 
